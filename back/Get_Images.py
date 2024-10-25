2f47915e8276801ac5e6a0b8bdb3f32c3b31fc13
Alireza Samanipour <alirezasamanipource@gmail.com>
2024-10-25 11:13:27 +0330

    improving getting image py code and add more products and thier imeges for encoding and train

--- back/Get_Images.py
+++ back/Get_Images.py
@@ -3,12 +3,13 @@
 import cv2
 import os
 
+# Number of cameras
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Unable to read camera feed!")
 
-output_dir = 'back\Images'
+output_dir = 'back\Getting Images'
 # os.makedirs(output_dir, exist_ok=True)
 image_counter = 0
 
@@ -19,7 +20,7 @@ while True:
     cv2.imshow('Webcam', frame)
     k = cv2.waitKey(1)
     if k % 256 == 27:
-        print("Escape hit, closing...")
+        print("Escape hit, closing...") #break poin of while
         break
     elif k % 256 == ord('s'):   # wait for 's' key to save and exit
         img_name = os.path.join(output_dir, "opencv_frame_{}.png".format(image_counter))
