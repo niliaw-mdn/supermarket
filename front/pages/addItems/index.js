d97ddf335808c1599ac2b44da48e754b15e03724
niliaw-mdn <niloofarmadani8@gmail.com>
2024-11-03 09:42:14 -0800

    update

--- front/pages/addItems/index.js
+++ front/pages/addItems/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { useRouter } from "next/router";
-
 import { IoArrowBackOutline } from "react-icons/io5";
 
-function index() {
+function Index() {
   const router = useRouter();
 
   const backHandler = () => {
@@ -33,7 +32,9 @@ function index() {
 
       <div className="flex flex-col justify-center items-center">
         <div className="bg-gray-300 h-auto w-[500px] rounded-lg flex flex-col justify-center items-center">
-        <div className="mb-4 font-semibold pt-5 pl-56">:مشخصات محصول را وارد کنید</div>
+          <div className="mb-4 font-semibold pt-5 pl-56">
+            :مشخصات محصول را وارد کنید
+          </div>
           <ul className="list-none" dir="rtl">
             <li className="py-5">
               <input
@@ -63,6 +64,20 @@ function index() {
                 type="text"
               />
             </li>
+            <li>
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900"
+                htmlFor="file_input"
+              >
+                عکس محصول:
+              </label>
+              <input
+                className="block w-full text-sm text-gray-900 border border-gray-500 rounded-lg cursor-pointer bg-gray-400 placeholder-gray-400"
+                id="file_input"
+                type="file"
+                accept="image/*"
+              />
+            </li>
             <li className="m-10 ml-10 bg-[#0f8515] text-center pt-1 rounded-md h-10">
               <button className="text-white font-bold text-lg">تایید</button>
             </li>
@@ -73,4 +88,4 @@ function index() {
   );
 }
 
-export default index;
+export default Index;
