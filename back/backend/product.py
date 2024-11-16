f27e1a078d290ec6d0245efd37015667c8fe9b46
Alireza Samanipour <alirezasamanipource@gmail.com>
2024-11-16 13:31:56 +0330

    add some functions

--- back/backend/product.py
+++ back/backend/product.py
@@ -1,3 +1,5 @@
+import cv2
+import os
 class Product:
     def __init__(self, name, manufacturer, weight, salePrice, purchasePrice,discountPercentage, imageData, voluminosity, combinations,
                 nutritionalInformation, expirationDate, storageConditions, availableQuantity, numberSold, dateAddedToStock, totalProfitOnSales):
@@ -35,8 +37,35 @@ class Product:
         pass
     
     # this function making taking photos easier for admin 
-    def addImagesForDataset():
-        pass
+    def addImagesForDataset(self):
+        # Number of cameras
+        cap = cv2.VideoCapture(0)
+
+        # Send error if camera is unavaileble
+        if not cap.isOpened():
+            print("Unable to read camera feed!")
+
+        output_dir = '\getting images'
+        # os.makedirs(output_dir, exist_ok=True)
+        image_counter = 0
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            cv2.imshow('Webcam', frame)
+            k = cv2.waitKey(1)
+            if k % 256 == 27:
+                print("Escape hit, closing...") #break poin of while
+                break
+            elif k % 256 == ord('s'):   # wait for 's' key to save and exit
+                img_name = os.path.join(output_dir, f"opencv_frame_{image_counter}.png")
+                cv2.imwrite(img_name, frame)
+                print(f"{img_name}written!")
+                image_counter += 1
+
+        cap.release()
+        cv2.destroyAllWindows()
     # this is returning all available quantity for product
     def inventory(self):
         return self.availableQuantity
