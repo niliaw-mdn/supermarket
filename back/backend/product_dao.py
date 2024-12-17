3b87108ffb6ce64d6751272a50f29933e28ed621
Alireza Samanipour <alirezasamanipource@gmail.com>
2024-12-17 09:29:41 +0330

    product clean

--- back/backend/product_dao.py
+++ back/backend/product_dao.py
@@ -4,10 +4,14 @@ from sql_connection import get_sql_connection
 
 def get_all_products(connection):
     cursor = connection.cursor()
+    
     query = """ SELECT  product.product_id, product.name, product.uom_id,
     product.price_per_unit, product.available_quantity, product.image_data,
     uom.uom_name FROM grocery_store.product inner join uom on product.uom_id=uom.uom_id;"""
+    
+    
     cursor.execute(query)
+    
     response = []
     for (product_id, name, uom_id, price_per_unit, available_quantity, uom_name, image_data) in cursor:
         response.append(
@@ -20,25 +24,30 @@ def get_all_products(connection):
                 'available_quantity' : available_quantity,
                 'image_data' : image_data
             }   
-        )        
+        )   
+    
     return response
 
 
 
 def get_product(connection, product_id):
     cursor = connection.cursor()
+    
     query = ("""SELECT product.product_id, product.name, product.uom_id, product.price_per_unit,
                 product.available_quantity, product.image_data, manufacturer_name, weight,
                 purchase_price, discount_percentage, voluminosity, combinations,
                 nutritional_information, expiration_date, storage_conditions, number_sold,
                 date_added_to_stock, total_profit_on_sales, error_rate_in_weight,
                 uom.uom_name FROM grocery_store.product inner join uom on product.uom_id=uom.uom_id WHERE product_id=""" + str(product_id))
+    
     cursor.execute(query)
+    
     response = []
     for (product_id, name, uom_id, price_per_unit, available_quantity, uom_name,
             image_data, manufacturer_name, weight, purchase_price, discount_percentage,
             voluminosity, combinations, nutritional_information, expiration_date,
             storage_conditions, number_sold, date_added_to_stock, total_profit_on_sales, error_rate_in_weight) in cursor:
+        
         response.append(
             {    
                 'product_id' : product_id,
@@ -62,40 +71,48 @@ def get_product(connection, product_id):
                 'total_profit_on_sales' : total_profit_on_sales,
                 'error_rate_in_weight' : error_rate_in_weight
             }   
-        )        
+        ) 
+    
     return response
-    
-    
+
+
+
 def insert_new_product(connection, product):
     cursor = connection.cursor()
+    
     query = ("""insert into product (name, uom_id, price_per_unit, available_quantity, image_data,
                 manufacturer_name, weight, purchase_price, discount_percentage, voluminosity,
                 combinations, nutritional_information, expiration_date, storage_conditions,
                 number_sold, date_added_to_stock, total_profit_on_sales, error_rate_in_weight)
                 values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""") # this line has too be complete
     
+    
     data = (product['product_name'], product['uom_id'], product['price_per_unit'], product['available_quantity'],
             product['image_data'], product['manufacturer_name'], product['weight'], product['purchase_price'], 
             product['discount_percentage'], product['voluminosity'], product['combinations'], product['nutritional_information'], 
             product['expiration_date'], product['storage_conditions'], product['number_sold'], product['date_added_to_stock'],
             product['total_profit_on_sales'], product['error_rate_in_weight'])
+    
     cursor.execute(query, data)
     connection.commit()
     
     return cursor.lastrowid
-    
-    
+
+
 def delete_product(connection, product_id):
     # sourcery skip: use-fstring-for-concatenation
     cursor = connection.cursor()
+    
     query = ("DELETE FROM product where product_id=" + str(product_id))
+    
     cursor.execute(query)
     connection.commit()
-    
-    
+
+
 
 def update_product(connection, product_id, product_data):
     cursor = connection.cursor() 
+    
     sql = """ UPDATE products SET product_name = %s,
                 uom_id = %s,
                 price_per_unit = %s,
@@ -116,6 +133,7 @@ def update_product(connection, product_id, product_data):
                 error_rate_in_weight = %s
                 WHERE product_id = %s """
     
+    
     values = ( product_data['product_name'], product_data['uom_id'], product_data['price_per_unit'],
                 product_data['available_quantity'], product_data['image_data'], product_data['manufacturer_name'],
                 product_data['weight'], product_data['purchase_price'], product_data['discount_percentage'],
@@ -123,18 +141,23 @@ def update_product(connection, product_id, product_data):
                 product_data['expiration_date'], product_data['storage_conditions'], product_data['number_sold'],
                 product_data['date_added_to_stock'], product_data['total_profit_on_sales'], product_data['error_rate_in_weight'],
                 product_id )
+    
+    
     cursor.execute(sql, values) 
     connection.commit() 
     cursor.close()
-    
-    
-    
+
+
+
 
 #have to be complete for availablity
 def get_available_quantity(connection, product_id):
     cursor = connection.cursor()
+    
     query = f"SELECT product.available_quantity FROM grocery_store.product WHERE product_id={[product_id]}"
+    
     cursor.execute(query)
+    
     return [
         {'available_quantity': available_quantity}
         for available_quantity in cursor
